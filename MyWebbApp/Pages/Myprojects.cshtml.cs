2e926f3ccde3bd14c818fc9964341672dd754051
Lacrom <80358801+Lacrom@users.noreply.github.com>
2022-07-17 22:31:32 +0200

    Add project files.

---
+++ MyWebbApp/Pages/Myprojects.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebbApp.Pages
+{
+    public class MyprojectsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
