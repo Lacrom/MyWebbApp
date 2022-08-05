26153c17c51ef6ee0da4c664b7018ddce9d11c2d
Kamil Wasiela <249659@student.pwr.edu.pl>
2022-08-06 00:01:17 +0200

    Some changes in Myprojects page - view depends on table from database.

--- MyWebbApp/Pages/Myprojects.cshtml.cs
+++ MyWebbApp/Pages/Myprojects.cshtml.cs
@@ -1,12 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyWebbApp.Data;
+using MyWebbApp.Models;
+using MyWebbApp.Pages.Admin_Pages;
 
 namespace MyWebbApp.Pages
 {
     public class MyprojectsModel : PageModel
     {
-        public void OnGet()
+        private readonly MyWebbApp.Data.MyWebbAppContext _context;
+
+        public MyprojectsModel(MyWebbApp.Data.MyWebbAppContext context)
         {
+            _context = context;
+        }
+
+
+        public IList<My_Projects_Model> My_Projects_Model { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.My_Projects_Model != null)
+            {
+                My_Projects_Model = await _context.My_Projects_Model.ToListAsync();
+            }
         }
     }
 }
